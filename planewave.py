7fb72e3e01ffe44ea0466b0fd07127053a27ef50
Joakim Pugesgaard Sandberg <s203937@dtu.dk>
2025-06-23 10:04:39 +0200

    polarizations added

--- planewave.py
+++ planewave.py
@@ -9,29 +9,49 @@ import mpl_toolkits.mplot3d.axes3d as axes3d
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 class planeWave(Multipoles):
-    def __init__(self, maxJ, wl, domain, polarization = "x"):
-        super().__init__(maxJ, maxJ, wl, domain, nr=1, radius=0.25)
-        self.wn = 2 * np.pi / wl
+    def __init__(self, wavelength, domain, polarization = "x"):
+        maxJ = 50
+        self.wl = wavelength
+        self.wn = 2 * np.pi / wavelength
         self.domain = domain
         self.size = domain.size
-        self.planes = self.spherical_grids.keys()
+        self.polarization = polarization
         self.maxJ = maxJ
         self.poltypes = ["x", "y", "left circular", "right circular"]
         if polarization not in self.poltypes:
             raise ValueError(f"polarization must be one of {self.poltypes}")
 
-        self.wl = wl
+        if polarization == "left circular":
+            self.p = 1#np.array([1])
+            self.super = 1
+            self.prefac = 1
+        elif polarization == "right circular":
+            self.p = -1#np.array([-1])
+            self.super = 1
+            self.prefac = 1
+        elif polarization == "x":
+            self.p = np.array([1, -1])
+            self.super = 1
+            self.prefac = 1/np.sqrt(2)
+            maxJ += 50
+        elif polarization == "y":
+            self.p = np.array([1, -1])
+            self.super = -1
+            self.prefac = 1/(np.sqrt(2)*1j)
+            maxJ += 50
+            
         self.mz_star = 1
-        
+        self.maxJ = maxJ
         # Initialize beam parameters dictionary
         self.beam_params = {
             'type': "planewave",
             'wl': self.wl,
             'mz_star': self.mz_star,
             'maxJ': self.maxJ,
-            'nr': self.nr,
          }
-        
+
+        super().__init__(maxJ, 1, wavelength, domain, nr=1, radius=0.25)
+        self.planes = self.spherical_grids.keys()
         # initialize arrays of spherical coordinates. shape = (Nplanes, shape(grid))
         self.R = np.array([self.spherical_grids[plane][0] for plane in self.planes])
         self.Theta = np.array([self.spherical_grids[plane][1] for plane in self.planes])
@@ -41,7 +61,8 @@ class planeWave(Multipoles):
         #definepute beam coefficients
         self.C = np.ones(self.maxJ + 1, dtype=complex)
 
-    
+        
+        
     def plot_Cjl(self):
         
         fig, ax = plt.subplots(1, 1, figsize=(8, 6))
@@ -57,90 +78,68 @@ class planeWave(Multipoles):
         fig.tight_layout()
         plt.show()
     
-    def compute_sum(self, l, p, q, spatial_fun = "bessel"):
+    def compute_sum(self, spatial_fun = "bessel"):
+        j0 = 1
         
-        m = l + p
+        mp0L = self.get_multipoles(j0, 1, spatial_fun)
+        mp0R = self.get_multipoles(j0, 1, spatial_fun)
         
-        j0 = max(abs(m), 1)
-
-        mp0 = self.get_multipoles(j0, m, spatial_fun)
-
-        mp0["magnetic"] *= self.C[j0]*(1j)**j0 * np.sqrt(2*j0+1)
-        mp0["electric"] *= self.C[j0]*(1j)**j0 * np.sqrt(2*j0+1)
-
-        for j in range(j0+1, self.maxJ+1):
-            mp = self.get_multipoles(j, m, spatial_fun)
+        sumL = np.zeros_like(mp0L["magnetic"], dtype=complex)
+        sumR = np.zeros_like(mp0L["magnetic"], dtype=complex)
+        
+        mp0L["magnetic"] *= 0
+        mp0L["electric"] *= 0
+        mp0R["magnetic"] *= 0
+        mp0R["electric"] *= 0
+        
+  
+        for j in range(j0, self.maxJ+1):
+            mpp = self.get_multipoles(j, 1, spatial_fun)
+            mpm = self.get_multipoles(j, -1, spatial_fun)
             
-            mp0["magnetic"] += (1j)**j * np.sqrt(2*j+1) * self.C[j] * mp["magnetic"]
-            mp0["electric"] += (1j)**j * np.sqrt(2*j+1) * self.C[j] * mp["electric"]
+            mp0L["magnetic"] += (1j)**j * np.sqrt(2*j+1) * mpp["magnetic"]
+            mp0L["electric"] += (1j)**j * np.sqrt(2*j+1) * mpp["electric"]
+            mp0R["magnetic"] += (1j)**j * np.sqrt(2*j+1) * mpm["magnetic"]
+            mp0R["electric"] += (1j)**j * np.sqrt(2*j+1) * mpm["electric"]
+            
+        sumL = sumL + np.sqrt(2*np.pi) * (mp0L["magnetic"]+(1j)*mp0L["electric"])
+        sumR = sumR + np.sqrt(2*np.pi) * (mp0R["magnetic"]-(1j)*mp0R["electric"])
+        
+        if self.polarization == "left circular":
+            sum = sumL
+            
+        elif self.polarization == "right circular":
+            sum = sumR
+        
+        if self.polarization == "x":
+            sum = (sumL + sumR) / np.sqrt(2)
+            
+        elif self.polarization == "y":
+            sum = (sumL - sumR) / (np.sqrt(2)*1j)
+        
+        return sum 
 
-        sum = mp0["magnetic"]+(1j)*p*mp0["electric"]
-
-        return sum
-
-    def plot_beam(self, l=None, p=None, q=None, interaction="scattering", plot="components", globalnorm=False):
+    def plot_beam(self, plot="components", globalnorm=False):
         """Plot the computed sum of multipoles.
 
         Args:
-            l (int): Orbital angular momentum quantum number.
-            p (int): Azimuthal quantum number.
-            q (int): Radial quantum number.
-            maxJ (int): Maximum value of the total angular momentum quantum number.
-            interaction (str, optional): The desired interaction to visualize. Can either be 'scattering' or 'internal', which will show the field respectively outside or inside 
-                                a central region. Defaults to "scattering".
             plot (str, optional): Plot full intensity or each polarization component. Defaults to "components".
             globalnorm (bool, optional): Normalize each plot to its own max (see individual behavior) or global max (see which components dominate). Defaults to False.
 
         Raises:
-            ValueError: interaction must be 'scattering', 'internal' or 'both'
             ValueError: plot must be 'components' or 'total'
         """
-        
-        assert interaction in ["scattering", "internal", "both"], "interaction must be 'scattering' or 'internal'"
+
         assert plot in ["components", "total"], "plot must be 'components' or 'total'"
-        
-        rr = 0.25 * self.size  # Define the radius of the sphere
-        
-        # Initialize SCA and ABS arrays with ones, same shape as R
-        SCA = np.ones_like(self.R)
-        ABS = np.ones_like(self.R)
-        
-        # Find indices where values are less than or greater than rr
-        index1 = np.where(self.R < rr)
-        index2 = np.where(self.R > rr)
+    
 
-        # Set values in ABS and SCA arrays based on indices
-        ABS[index2] = 0
-        SCA[index1] = 0
-        
-        if interaction == "scattering":
-            spatial_fun = "hankel"
-            S = SCA
-        elif interaction == "internal":
-            spatial_fun = "bessel"
-            S = ABS
-        elif interaction == "both":
-            spatial_fun = "bessel"
-            S = np.ones_like(self.R)
-        else:
-            raise ValueError("interaction must be 'scattering' or 'internal'")
-        
-        if  (l is not None) or (p is not None) or (q is not None):
-            if p not in [-1,1]:
-                raise ValueError("p must be either -1 or 1")
-            # If l, p, q are provided, set new C
-            self.l = l
-            self.p = p
-            self.q = q
-            self.C, self.lensInt, self.suma = self.C_jlp(l=l, p=p, q=q)
-
-        sum = self.compute_sum(self.l, self.p, self.q, spatial_fun)
+        sum = self.compute_sum()
         
         if plot == "components":
             # Plot Nself.planes x 3 subplots
             fig, axs = plt.subplots(len(self.planes), 3, figsize=(12, 4 * len(self.planes)))
-            
-            sum[:] = np.abs(sum[:])**2 * S
+            sum[:] = np.real(sum[:]) 
+                
             if globalnorm:
                 # Find the global min and max values for normalization
                 vmin = np.min(np.abs(sum[:])) 
@@ -149,7 +148,7 @@ class planeWave(Multipoles):
             else:
                 norm = None
                 
-            fig.suptitle(f'Computed Sum (l={self.l}, p={self.p}, q={self.q})', fontsize=24, fontweight='bold')
+            fig.suptitle(f'Real parts of polarization components', fontsize=24, fontweight='bold')
             for i, plane in enumerate(self.planes):
 
                 im0 = axs[i, 0].imshow(np.abs(sum[0][i]).T, extent=(-self.size, self.size, -self.size, self.size), origin='lower', cmap='hot', norm=norm)  
@@ -181,14 +180,12 @@ class planeWave(Multipoles):
             fig.subplots_adjust(hspace=-0.8, wspace=-0.2)
             fig.tight_layout()
             plt.show()
-        
+
         elif plot == "total":
             # Plot self.planes x 1 subplots with the summed intensity of the three components
             fig, axs = plt.subplots(len(self.planes), 1, figsize=(12, 4 * len(self.planes)))
 
-            # Sum the intensities of the three components
-            total_intensity = np.sum(np.abs(sum)**2 * S, axis=0)
-
+            total_intensity = np.sum(np.abs(np.real(sum[:])**2) , axis=0)
             if globalnorm:
                 # Find the global min and max values for normalization
                 vmin = np.min(total_intensity)
@@ -197,7 +194,7 @@ class planeWave(Multipoles):
             else:
                 norm = None
 
-            fig.suptitle(f'Total Intensity (l={self.l}, p={self.p}, q={self.q})', fontsize=24, fontweight='bold')
+            fig.suptitle(r'Real part of total intensity', fontsize=24, fontweight='bold')
             for i, plane in enumerate(self.planes):
                 im = axs[i].imshow(total_intensity[i].T, extent=(-self.size, self.size, -self.size, self.size), origin='lower', cmap='hot', norm=norm)
                 axs[i].set_title(f'{plane} plane')
